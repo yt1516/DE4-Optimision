c6b1acbefd58d6ac0e0fe5995db365ce37d4587e
SWillis97 <45208180+SWillis97@users.noreply.github.com>
2019-12-13 15:23:38 +0000

    Add files via upload

--- Sam_Submission/Master_Code_Looped.m
+++ Sam_Submission/Master_Code_Looped.m
@@ -1,26 +1,47 @@
-clc
-clear all
+function y = blkdiag(varargin)
+%BLKDIAG  Block diagonal concatenation of matrix input arguments.
+%
+%                                   |A 0 .. 0|
+%   Y = BLKDIAG(A,B,...)  produces  |0 B .. 0|
+%                                   |0 0 ..  |
+%
+%   Class support for inputs:
+%      float: double, single
+%      integer: uint8, int8, uint16, int16, uint32, int32, uint64, int64
+%      char, logical
+%
+%   See also DIAG, HORZCAT, VERTCAT
 
-analysed_profiles = 50:5:100; %defining the initial spread of profiles
-profile_space = 5;
+% Copyright 1984-2013 The MathWorks, Inc.
 
-L = 3;
+if nargin > 0
+    outclass = class(varargin{1});
+    if isobject(varargin{1}) || ~isnumeric(varargin{1}) || ...
+            any(~cellfun('isclass',varargin,outclass))
+        y = [];
+        for k=1:nargin
+            x = varargin{k};
+            [p1,m1] = size(y); 
+            [p2,m2] = size(x);
+            y = [y zeros(p1,m2); zeros(p2,m1) x]; %#ok
+        end
+        return
+    end
+else
+   outclass = 'double';  
+end
 
-for i = 1:L
+if any(cellfun(@issparse,varargin));
+    % optimized MEX implementation for sparse double
+    y = blkdiagmex(varargin{:});
+else
+    [p2,m2] = cellfun(@size,varargin);
+    %Precompute cumulative matrix sizes
+    p1 = [0, cumsum(p2)];
+    m1 = [0, cumsum(m2)];
     
-    %Jacob, replace the Pn input with your output and Load_perhouse with
-    %your input for load
-    WTFout_Combined = matfile('JACOB_11_Profiles.mat');
-    Pn  = WTFout_Combined.Complete_Pout;
-    Load_perhouse = csvread('JACOB_Load_Use.csv',1,1);
-    Turbine_Base = matfile('JACOB_11_Profiles.mat');
-    Turbine_Base = Turbine_Base.c_out_sam;
-
-    [optimal_Cost, wind_turbine_profile, ratio] = SAM_Opt_One_Profit(Pn, Load_perhouse, Turbine_Base);
-
-    profile_space = profile_space/((i)*11); %shrink the profile space
-    
-    analysed_profiles = (analysed_profiles(wind_turbine_profile)-(profile_space*11)):profile_space:(analysed_profiles(wind_turbine_profile));
-    % The new profile to be investigated
-    
+    y = zeros(p1(end),m1(end),outclass); %Preallocate
+    for k=1:nargin
+        y(p1(k)+1:p1(k+1),m1(k)+1:m1(k+1)) = varargin{k};
+    end
 end
